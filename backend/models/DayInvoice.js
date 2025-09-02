7bc4bc56dc46065f7e0e6db555c40c873866d3fc
Hemanth Sathyaseelan <hemanth@bizalign.co.uk>
2025-09-02 11:44:51 +0100

    Personnel model update

--- backend/models/DayInvoice.js
+++ backend/models/DayInvoice.js
@@ -3,21 +3,11 @@ const DeductionSchema = require('./Deduction').schema;
 const IncentiveSchema = require('./Incentive').schema
 
 const dayInvoiceSchema = new mongoose.Schema({
-    personnelId: { type: String, required: true },
+    personnelId: { type: mongoose.Schema.Types.ObjectId, ref: 'Personnel', required: true },
     user_ID: { type: String, required: true },
-    personnelName: { type: String, required: true },
-    personnelEmail: { type: String, required: true },
     date: { type: Date, required: true },
     role: { type: String, required: true },
     week: { type: String, required: true },
-    incentiveDetail: {
-      type: [DeductionSchema],
-      default: []
-    },
-    deductionDetail: {
-      type: [DeductionSchema],
-      default: []
-    },
     total: { type: Number, required: true },
     addedBy: { type: Object },
     modifiedBy: { type: Object },
