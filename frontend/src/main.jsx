65e70b8167cb0e2e7c696e93e43d9b0b76870d8c
Sanjaykumar Ramachandran <ramachandransanjaykumar@gmail.com>
2025-09-05 11:26:33 +0100

    day and weeklyinvoice gen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './lib/themeLoader'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
